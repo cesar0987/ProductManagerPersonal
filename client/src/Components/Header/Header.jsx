a8f42f4670cbb7e2cc52f2ad3804ea73d77b9103
cesar0987 <cesarbenitez46@gmail.com>
2024-06-17 21:57:11 -0400

    feat:Changes on Inventory

--- client/src/Components/Header/Header.jsx
+++ client/src/Components/Header/Header.jsx
@@ -1,6 +1,10 @@
 import "./Header.css"
-
+import { useLocation } from "react-router-dom";
 export const Header = () => {
+    const location = useLocation();
+    
+
+    console.log(location);
     return (
         <div className="headerContainer">
             <div className="headerTitle">
@@ -8,8 +12,7 @@ export const Header = () => {
             <h2>Inventory Management System</h2>
             </div>
             <form>
-            <input type="text" placeholder="Search" />
-            <button>Search</button>
+            {(location.pathname === "/") ? <> <input type="text" placeholder="Search" /> <button type="submit">Search</button></> : null}
             </form>
         </div>
     );
